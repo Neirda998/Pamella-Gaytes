aaf64d9dc774ff395dd80825b98aab95f12668b3
Ailios973 <adrien.tarcy@epita.fr>
2017-01-15 00:21:54 +0100

    Unity project
    j'ai fais mon propre dossier uniy avec les assets que j'ai créer essayez
    de tester si ca marche

---
+++ Pamella Gaytes/Assets/Created Assets/Scripts/ControlPLateformer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlPlateformer : MonoBehaviour {
+
+    public float speed = 0.20f;
+    public float jump = 4.01f;
+
+	// Use this for initialization
+	void Start () {
+        print("Start");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 dp = new Vector3();
+        if (Input.GetKey(KeyCode.Q))
+        {
+            dp.x -= speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            dp.x += speed;
+        }
+
+        transform.position += dp;
+	}
+}
