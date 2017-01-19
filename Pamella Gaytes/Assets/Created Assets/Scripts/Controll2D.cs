4940382057b0a5b581b39c24f98426378ac73e00
Ailios973 <adrien.tarcy@epita.fr>
2017-01-19 01:58:21 +0100

    Moving
    Smooth as F... just the jump

---
+++ Pamella Gaytes/Assets/Created Assets/Scripts/Controll2D.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controll2D : MonoBehaviour {
+    public float godspeed = 10f;
+    private Rigidbody rig;
+    
+
+	// Use this for initialization
+	void Start ()
+    {
+        rig = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        float axeh = Input.GetAxis("Horizontal");
+        Vector3 movement = new Vector3(axeh, 0, 0) * godspeed * Time.deltaTime;
+        rig.MovePosition(transform.position + movement);
+
+    }
+}
