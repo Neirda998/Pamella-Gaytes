15f6241674e201e4db1b01e73716172f07c48b66
Ailios973 <adrien.tarcy@epita.fr>
2017-02-08 19:39:47 +0100

    Fixing

---
+++ Pamella Gaytes/Assets/Created Assets/Scripts/CameraControler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public float lerpSpeed;
+    private void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, new Vector3(target.position.x, target.position.y, transform.position.z), Time.deltaTime * lerpSpeed);
+    }
+
+}
