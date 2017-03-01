2fe0e6f38deda0d84855679b569b0e7adfbc8729
Neirda998 <adrijosse@hotmail.fr>
2017-03-01 21:36:14 +0100

    finished texture in Pamelite

---
+++ Pamelite/Assets/SpinScie.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinScie : MonoBehaviour {
+    public float speed = 100f;
+    
+    void Update()
+    {
+        transform.Rotate(0, Time.deltaTime * speed, 0, Space.World);
+    }
+}
