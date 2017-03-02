159d64e61aff035532d9ddc09b88381fa63e469f
Neirda998 <adrijosse@hotmail.fr>
2017-03-02 13:39:06 +0100

    Adding GameOver and Pause + spin scie

---
+++ Pamelite/Assets/Created Assets/Scripts/SpinScieUpDown.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinScieUpDown : MonoBehaviour {
+    void Start()
+    {
+
+    }
+    private bool down = true;
+    private Vector3 targetup = new Vector3(0, 14, 0);
+    private Vector3 targetdown = new Vector3(0, 9, 0);
+
+    void Update ()
+    {
+        transform.Rotate(0, 0, Time.deltaTime * 100000);
+        if (down)
+        {
+            transform.Translate(Vector3.up * Time.deltaTime, Space.World);
+            if(targetup.y - transform.position.y <= 0)
+            {
+                down = false;
+            }
+        }
+        if (!down)
+        {
+            transform.Translate(Vector3.down * Time.deltaTime, Space.World);
+            if (targetdown.y - transform.position.y >= 0)
+            {
+                down = true;
+            }
+        }
+    }
+}
