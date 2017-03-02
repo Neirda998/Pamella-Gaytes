52ea9d543cd160e6980dc779b4233883abcb9293
Ailios973 <adrien.tarcy@epita.fr>
2017-03-02 17:33:46 +0100

    HUB

---
+++ Pamella Gaytes/Assets/Created Assets/Scripts/SecurityCamera.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecurityCamera : MonoBehaviour
+{
+    public Transform target;
+    public float lerpSpeed;
+    private void LateUpdate()
+    {
+        transform.LookAt(target);
+    }
+
+}
