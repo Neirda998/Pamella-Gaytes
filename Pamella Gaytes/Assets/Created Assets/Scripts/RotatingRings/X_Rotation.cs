31cf84d241d46bcce93c2eb6f5f5d93c783f394a
Neirda998 <adrijosse@hotmail.fr>
2017-04-14 22:25:55 +0200

    Level4 debut

---
+++ Pamella Gaytes/Assets/Created Assets/Scripts/RotatingRings/X_Rotation.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class X_Rotation : MonoBehaviour {
+    void Update()
+    {
+        transform.Rotate(Time.deltaTime * 50, 0, 0);
+    }
+}
---
+++ Pamella Gaytes/Assets/Created Assets/Scripts/RotatingRings/Y_Rotation.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Y_Rotation : MonoBehaviour {
+    void Update()
+    {
+        transform.Rotate(0, Time.deltaTime * 50, 0);
+    }
+}
