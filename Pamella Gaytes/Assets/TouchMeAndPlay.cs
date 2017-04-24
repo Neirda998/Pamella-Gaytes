e193e2c58d97a35c7c2db7d383e594b0fc807473
Sgrt <sebastien.gueret@epita.fr>
2017-04-24 03:18:48 +0200

    Box Hub

---
+++ Pamella Gaytes/Assets/TouchMeAndPlay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TouchMeAndPlay : MonoBehaviour
+{
+
+    public GameObject entry1;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            entry1.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+
+}
+
