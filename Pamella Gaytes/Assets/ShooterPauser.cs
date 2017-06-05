5c029fcf4b98f3fcf4df2d07a39d333c9c8d7b78
Ailios973 <adrien.tarcy@epita.fr>
2017-06-05 14:36:05 +0200

    Pausing the shooter

---
+++ Pamella Gaytes/Assets/ShooterPauser.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ShooterPauser : MonoBehaviour {
+
+    public Transform canvas;
+
+    public GameObject player;
+    public GameObject background;
+    public GameObject gameController;
+
+    [SerializeField]
+    string scene = SceneManager.GetActiveScene().ToString();
+
+	// Update is called once per frame
+	void Update ()
+    {
+		Pause();
+	}
+
+    public void Pause()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (canvas.gameObject.activeInHierarchy == false)
+            {
+                canvas.gameObject.SetActive(true);
+                Time.timeScale = 0;
+                player.GetComponent<Done_PlayerController>().enabled = false;
+                background.GetComponent<Done_BGScroller>().enabled = false;
+                gameController.GetComponent<AudioSource>().enabled = false;
+
+            }
+            else
+            {
+                canvas.gameObject.SetActive(false);
+                Time.timeScale = 1;
+                player.GetComponent<Done_PlayerController>().enabled = true;
+                background.GetComponent<Done_BGScroller>().enabled = true;
+                gameController.GetComponent<AudioSource>().enabled = true;
+            }
+        }
+    }
+    public void GoToHub()
+    {
+        SceneManager.LoadScene(0);
+        player.GetComponent<Done_PlayerController>().enabled = true;
+        background.GetComponent<Done_BGScroller>().enabled = true;
+        gameController.GetComponent<AudioSource>().enabled = true;
+        Time.timeScale = 1;
+    }
+
+    public void Retry()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        player.GetComponent<Done_PlayerController>().enabled = true;
+        background.GetComponent<Done_BGScroller>().enabled = true;
+        gameController.GetComponent<AudioSource>().enabled = true;
+        Time.timeScale = 1;
+    }
+    public void IQuit()
+    {
+        Application.Quit();
+    }
+}
